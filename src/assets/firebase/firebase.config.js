cf2d217706523607c336bb1a9d958eb67c1a7c50
khairulkhn <khairulkhn@gmail.com>
2023-04-29 15:19:34 +0600

    hide firebase config

---
+++ src/assets/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDzNOuLhAo0HqX4w197u0h3DFZ3eAv460",
+  authDomain: "the-news-dragon-38563.firebaseapp.com",
+  projectId: "the-news-dragon-38563",
+  storageBucket: "the-news-dragon-38563.appspot.com",
+  messagingSenderId: "700230626819",
+  appId: "1:700230626819:web:1c272c22fda2a2a0ebe836"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
